f1f7f2a5371a8f14cce4e6124b2dd2803e365a40
trakkito <158862513+trakkito@users.noreply.github.com>
2024-07-21 14:24:34 -0400

    added ui to rock paper scissors project

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-//Declare getComputerAnswer function to get CPU answer
-//Define computerValue variable for the returned value
-//Code if-else statement to return "rock", "paper", or "scissors" based on returned value
-//Declare getUserAnswer function to get User answer
-//Define userInput variable for User input
-//Use prompt() to get value from user
-//Use toLowerCase() to ensure every entered input will return lowercase to avoid case sensitivity problems
-//Assign getComputerAnswer() and getUserAnswer() to computerAnswer and userAnswer variables
-//Define userScore and computerScore variables to get the scores of each player
-//Define currentRound variable to keep track of game rounds
-//Declare playRound function with if-else statements to determine the outcomes of each round by incrementing the userScore or computerScore based on the given value, incrementing the round, and returning an alert message based on the result.
-//Declare playGame function and nest userScore, computerScore, and currentRound variables, and the playRound() function inside
-//Create do-while loop inside of playGame() function in order to have the game run for 5 rounds and stops after that
-//Create if-else statement to output game results based on if the userScore or computerScore reaches 3 or more.
+// will update notes at future time
+
+const body = document.querySelector("body");
+
+const rock = document.createElement("button");
+const paper = document.createElement("button");
+const scissors = document.createElement("button");
+rock.textContent = "Rock";
+paper.textContent = "Paper";
+scissors.textContent = "Scissors";
+
+rock.addEventListener('click', () => {
+   playRound('rock');
+});
+
+paper.addEventListener('click', () => {
+    playRound('paper');
+});
+
+scissors.addEventListener('click', () => {
+   playRound('scissors');
+});
+
+body.appendChild(rock);
+body.appendChild(paper);
+body.appendChild(scissors);
+
+const roundResults = document.createElement("div");
+const cpuScore = document.createElement("div");
+const playerScore = document.createElement("div");
+
+body.appendChild(roundResults);
+body.appendChild(cpuScore);
+body.appendChild(playerScore);
 
 function getComputerAnswer() {
 let computerValue = Math.floor(Math.random() * 3);
@@ -26,61 +46,66 @@ if (computerValue === 0) {
 }
 }
 
-function getUserAnswer() {
-    let userInput = prompt("Rock, Paper, Scissors, SHOOT!");
-    return userInput.toLowerCase();
-}
+const disableButtons = () => {
+    rock.disabled = true;
+    paper.disabled = true;
+    scissors.disabled = true;
+};
 
-
-function playGame() {
 let userScore = 0
 let computerScore = 0
-let currentRound = 0
-    function playRound() {
+    function playRound(userAnswer) {
         const computerAnswer = getComputerAnswer()
-        const userAnswer = getUserAnswer()
         if (userAnswer === "rock" && computerAnswer === "scissors") {
             userScore++;
-            currentRound++;
-            alert("You won this round!");
+            roundResults.textContent = "You won this round!";
         } else if (userAnswer === "rock" && computerAnswer === "paper") {
             computerScore++;
-            currentRound++;
-            alert("You lost this round!");
+            roundResults.textContent = "You lost this round!";
         } else if (userAnswer === "rock" && computerAnswer === "rock") {
-            alert("Tie! Go Again.");
+            roundResults.textContent = "Tie! Go Again!";
         }  else if (userAnswer === "scissors" && computerAnswer === "paper") {
             userScore++;
-            currentRound++;
-            alert("You won this round!");
+            roundResults.textContent = "You won this round!";
         }  else if (userAnswer === "scissors" && computerAnswer === "rock") {
             computerScore++;
-            currentRound++;
-            alert("You lost this round!");
+            roundResults.textContent = "You lost this round!";
         }  else if (userAnswer === "scissors" && computerAnswer === "scissors") {
-            alert("Tie! Go Again.");
+            roundResults.textContent = "Tie! Go Again!";
         }  else if (userAnswer === "paper" && computerAnswer === "rock") {
             userScore++;
-            currentRound++;
-            alert("You won this round!");
+            roundResults.textContent = "You won this round!";
         }  else if (userAnswer === "paper" && computerAnswer === "scissors") {
             computerScore++;
-            currentRound++;
-            alert("You lost this round!");
+            roundResults.textContent = "You lost this round!";
         }  else if (userAnswer === "paper" && computerAnswer === "paper") {
-            alert("Tie! Go Again.");
+            roundResults.textContent = "Tie! Go Again!";
         } else {
-            alert("Not a valid outcome!");
+            roundResults.textContent = "Not a Valid Outcome!";
         }
-    }
-    do {
-        playRound();
-    } while (currentRound < 5);
-    if (userScore >= 3) {
-        alert("Congrats, you won!");
-    } else if (computerScore >= 3) {
-        alert("Sorry, you lose!")
-    }
+
+        cpuScore.textContent = "Computer Score: " + computerScore;
+        playerScore.textContent = "Player Score: " + userScore;
+
+        if (userScore === 5) {
+            roundResults.textContent = "Congrats, you won the game!";
+            disableButtons();
+        } else if (computerScore === 5) {
+            roundResults.textContent = "Sorry, you lost the game.";
+            disableButtons();
+        };
+};
+
+initializeGame = () => {
+    userScore = 0;
+    computerScore = 0;
+    rock.disabled = false;
+    paper.disabled = false;
+    scissors.disabled = false;
+    roundResults.textContent = "";
+    cpuScore.textContent = "Computer Score: 0";
+    playerScore.textContent = "Player Score: 0";
 }
 
-console.log(playGame())
+initializeGame();
+
